201097e4b3f25ed160425a181d289cdb0a264c06
Victorias1905 <victoria.strahova@student.uhasselt.be>
2023-05-25 15:29:41 +0300

    4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,4 @@
   let size = 100; // You can change this to the size you want for the square
 </script>
 
-<style>
-  .square {
-    width: {size}px;
-    height: {size}px;
-    background-color: red;
-  }
-</style>
-
-<div class="square"></div>
+<div style="width: {size}px; height: {size}px; background-color: red;"></div>
